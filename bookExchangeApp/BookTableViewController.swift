5db4116d09d84e3f4a0993b22d788b4d74840c2d
15217035 <15217035@cs12212.comp.hkbu.edu.hk>
2018-11-15 19:57:08 +0800

    first commit

---
+++ bookExchangeApp/BookTableViewController.swift
@@ -0,0 +1,115 @@
+//
+//  BookTableViewController.swift
+//  bookExchangeApp
+//
+//  Created by 15217035 on 15/11/2018.
+//  Copyright © 2018 comp4097proj. All rights reserved.
+//
+
+import UIKit
+
+struct deptInfo {
+    
+    var name:String
+    var book_id:Int
+    
+}
+
+
+
+class BookTableViewController: UITableViewController {
+    var bookArray = [deptInfo]()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        bookArray.append( deptInfo(name: "Computer Science", book_id: 1 ) )
+        
+        bookArray.append( deptInfo(name: "Communcation Studies", book_id: 2 ) )
+        
+        bookArray.append( deptInfo(name: "Physics", book_id: 3 ) )
+        
+        bookArray.append( deptInfo(name: "Chemistry", book_id: 4 ) )
+        
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return bookArray.count
+    }
+
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "bookCell", for: indexPath)
+
+        // Configure the cell...
+        cell.textLabel?.text = bookArray[indexPath.row].name
+
+        return cell
+    }
+    
+
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // Delete the row from the data source
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        } else if editingStyle == .insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
