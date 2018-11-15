0cf3c259dcfdd3e4dd8c82f9093efd475f615385
15217035 <15217035@cs12212.comp.hkbu.edu.hk>
2018-11-15 19:22:50 +0800

    Initial Commit

---
+++ bookExchangeApp/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  bookExchangeApp
+//
+//  Created by 15217035 on 15/11/2018.
+//  Copyright © 2018 comp4097proj. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
