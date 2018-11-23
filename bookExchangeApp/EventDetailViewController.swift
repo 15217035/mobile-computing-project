f1a996fb190d156f7623a7bce5d854101def456c
15217035 <15217035@cs12171.comp.hkbu.edu.hk>
2018-11-23 11:57:46 +0800

    add to calendar with time

--- bookExchangeApp/EventDetailViewController.swift
+++ bookExchangeApp/EventDetailViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Firebase
 import FirebaseFirestore
 
 import EventKit
@@ -15,18 +16,92 @@ class EventDetailViewController: UIViewController {
     
     var event_id:String = "empty"
     var event_name:String = "empty"
+    var start_time:String = "empty"
+    var end_time:String = "empty"
     
     
     // for next page addr
     var event_addr:GeoPoint = GeoPoint(latitude: 0.0, longitude: 0.0)
     var locationName:String = "error"
     
+    // for labelUI
     @IBOutlet weak var eventName: UILabel!
     
     
+    // for button
     @IBAction func clickToAddCal(_ sender: Any) {
          checkCalendarAuthorizationStatus()
     }
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        
+        let db = Firestore.firestore()
+        
+        let docRef = db.collection("Events").document(event_id)
+        
+        docRef.getDocument { (document, error) in
+            if let document = document, document.exists {
+//                let dataDescription = document.data().map(String.init(describing:)) ?? "nil"
+//                print("Document data: \(dataDescription)")
+                
+                if let name = document.data()?["name"],
+                let location = document.data()?["location"],
+                let startTime = document.data()?["startTime"],
+                let endTime = document.data()?["endTime"],
+                let startDate = document.data()?["startDate"],
+                let endDate = document.data()?["endDate"]{
+                    
+                    self.eventName.text = "\(name)"
+                    
+                    self.event_name = "\(name)"
+                    self.start_time = "\(startDate) \(startTime)"
+                    self.end_time = "\(endDate) \(endTime)"
+                    
+                    self.event_addr = document.data()?["address"] as! GeoPoint
+                    self.locationName = "\(location)"
+                    
+
+                }
+                
+            } else {
+                print("Document does not exist")
+            }
+        }
+        
+        
+        // Points to the root reference
+        let storageRef = Storage.storage().reference()
+        
+        // Points to "images"
+        let imagesRef = storageRef.child("images")
+        
+        // Points to "images/space.jpg"
+        // Note that you can use variables to create child values
+        let fileName = "space.jpg"
+        let spaceRef = imagesRef.child(fileName)
+        
+        // File path is "images/space.jpg"
+        let path = spaceRef.fullPath;
+        
+        // File name is "space.jpg"
+        let name = spaceRef.name;
+        
+        // Points to "images"
+        let images = spaceRef.parent()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
     
     func requestAccessToCalendar() {
         
@@ -38,7 +113,7 @@ class EventDetailViewController: UIViewController {
             if accessGranted == true {
                 DispatchQueue.main.async(execute: {
                     self.loadCalendars()
-//                    self.refreshTableView()
+                    //                    self.refreshTableView()
                 })
             } else {
                 DispatchQueue.main.async(execute: {
@@ -58,7 +133,7 @@ class EventDetailViewController: UIViewController {
         case EKAuthorizationStatus.authorized:
             // Things are in line with being able to show the calendars in the table view
             loadCalendars()
-//            refreshTableView()
+        //            refreshTableView()
         case EKAuthorizationStatus.restricted, EKAuthorizationStatus.denied:
             // We need to help them give us permission
             needPermissionView()
@@ -66,6 +141,7 @@ class EventDetailViewController: UIViewController {
     }
     
     func needPermissionView(){
+        // alert to show fail
         let alert = UIAlertController(title: "Alert", message: "We need the permission to Your Calendar", preferredStyle: UIAlertControllerStyle.alert)
         alert.addAction(UIAlertAction(title: "I'll do it later", style: UIAlertActionStyle.default, handler: nil))
         self.present(alert, animated: true, completion: nil)
@@ -73,19 +149,36 @@ class EventDetailViewController: UIViewController {
     
     
     func loadCalendars(){
+//        let startdate:Date
+//        let enddate:Date
+//        if let unixTimestamp = Double(start_time){
+//         startdate = Date(timeIntervalSince1970: unixTimestamp)
+//        }
+//
+//        if let unixTimestamp2 = Double(end_time){
+//         enddate = Date(timeIntervalSince1970: unixTimestamp2)
+//        }
+        
+        
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy/MM/dd HH:mm"
+        let someDateTime = formatter.date(from: start_time)
+        
+        let someDateTime2 = formatter.date(from: end_time)
+        
         let eventStore : EKEventStore = EKEventStore()
         
         eventStore.requestAccess(to: .event) { (granted, error) in
             
             if (granted) && (error == nil) {
                 print("granted \(granted)")
-                print("error \(error)")
+                print("error \(String(describing: error))")
                 
                 let event:EKEvent = EKEvent(eventStore: eventStore)
                 
-                event.title = "Test Title"
-                event.startDate = Date()
-                event.endDate = Date()
+                event.title = self.event_name
+                event.startDate = someDateTime
+                event.endDate = someDateTime2
                 event.notes = "This is a note"
                 event.calendar = eventStore.defaultCalendarForNewEvents
                 do {
@@ -94,51 +187,17 @@ class EventDetailViewController: UIViewController {
                     print("failed to save event with error : \(error)")
                 }
                 print("Saved Event")
+                
+                // alert to show success
+                let alert = UIAlertController(title: "You have successfully added", message: "Please go to Calendar and check", preferredStyle: UIAlertControllerStyle.alert)
+                alert.addAction(UIAlertAction(title: "OK!", style: UIAlertActionStyle.default, handler: nil))
+                self.present(alert, animated: true, completion: nil)
             }
             else{
                 
-                print("failed to save event with error : \(error) or access not granted")
+                print("failed to save event with error : \(String(describing: error)) or access not granted")
             }
         }
-    }
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        
-        let db = Firestore.firestore()
-        
-        let docRef = db.collection("Events").document(event_id)
-        
-        docRef.getDocument { (document, error) in
-            if let document = document, document.exists {
-//                let dataDescription = document.data().map(String.init(describing:)) ?? "nil"
-//                print("Document data: \(dataDescription)")
-                
-                if let name = document.data()?["name"],
-                    let location = document.data()?["location"]{
-                    
-                    self.eventName.text = "\(name)"
-                    
-                    self.event_name = "\(name)"
-                    
-                    self.event_addr = document.data()?["address"] as! GeoPoint
-                    self.locationName = "\(location)"
-                    
-
-                }
-                
-            } else {
-                print("Document does not exist")
-            }
-        }
-
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
     
 
