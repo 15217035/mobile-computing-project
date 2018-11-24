baa2fa805937396939fe6bb1131f5f673c51eed4
15217035 <15217035@cs12202.comp.hkbu.edu.hk>
2018-11-24 17:29:34 +0800

    upload library image to firebase

--- bookExchangeApp/AddNewBookViewController.swift
+++ bookExchangeApp/AddNewBookViewController.swift
@@ -10,14 +10,21 @@ import UIKit
 import FirebaseAuth
 import Firebase
 import FirebaseFirestore
+import Photos
 
 class AddNewBookViewController: UIViewController {
 
     @IBOutlet var bookNameTF: UITextField!
     @IBOutlet var authorTF: UITextField!
     
-   
-   var userID = ""
+    @IBOutlet weak var checkPhotoUpload: UILabel!
+    @IBOutlet weak var photoUploadImage: UIImageView!
+    
+    var userID = ""
+    
+    var image:UIImage = UIImage()
+    
+    let imagePicker = UIImagePickerController()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -29,13 +36,15 @@ class AddNewBookViewController: UIViewController {
     }
     
     @IBAction func addBookClicked(_ sender: Any) {
+        let image_set_name:String = "\(self.userID) \(bookNameTF.text ?? "123").jpg"
         
         var ref: DocumentReference? = nil
         ref = Firestore.firestore().collection("Books").addDocument(data: [
             "name": bookNameTF.text ?? "",
             "author": authorTF.text ?? "",
             "ownerId": self.userID,
-            "book_id": ""
+            "book_id": "",
+            "image": image_set_name
         ]) { err in
             if let err = err {
                 print("Error adding document: \(err)")
@@ -46,7 +55,114 @@ class AddNewBookViewController: UIViewController {
                 self.present(alertController, animated: true, completion: nil)
             }
         }
+        
+       
+
+        let imageData: Data = UIImagePNGRepresentation(image)!
+//        UIImage(data:imageData,scale:1.0)
+        
+        // Create a reference to the file you want to upload
+        let storageRef = Storage.storage().reference()
+        let folderRef = storageRef.child("book")
+        let riversRef = folderRef.child(image_set_name)
+        
+        // Upload the file to the path "images/rivers.jpg"
+        _ = riversRef.putData(imageData, metadata: nil) { metadata, error in
+            guard let metadata = metadata else {
+                // Uh-oh, an error occurred!
+                return
+            }
+            // Metadata contains file metadata such as size, content-type.
+            _ = metadata.size
+            // You can also access to download URL after upload.
+            storageRef.downloadURL { (url, error) in
+                guard let downloadURL = url else {
+                    // Uh-oh, an error occurred!
+                    return
+                }
+            }
+        }
     }
 
+    @IBAction func addPhotoFromLibary(_ sender: Any) {
+        
+        let photoAuthorizationStatus = PHPhotoLibrary.authorizationStatus()
+        switch photoAuthorizationStatus {
+            case .authorized:
+                self.addPhotoFromLibaryGo()
+                print("Access is granted by user")
+            case .notDetermined:
+                PHPhotoLibrary.requestAuthorization({ (newStatus) in print("status is \(newStatus)")
+                    if newStatus == PHAuthorizationStatus.authorized {
+                        print("success")
+                        self.addPhotoFromLibaryGo()
+                    }
+                })
+            case .restricted:
+                print("User do not have access to photo album.")
+            case .denied:
+                print("User has denied the permission.")
+            
+        }
+        
+    }
+        
+    func addPhotoFromLibaryGo(){
+        guard UIImagePickerController.isSourceTypeAvailable(.photoLibrary) else {
+              print("can't open photo library")
+              return
+        }
+        
+        imagePicker.sourceType = .photoLibrary
+        imagePicker.delegate = self
+        
+        present(imagePicker, animated: true)
+    }
+    
+    // unfinished
+    @IBAction func addPhotoFromCamera(_ sender: Any) {
+        guard UIImagePickerController.isSourceTypeAvailable(.camera) else {
+            print("camera not supported by this device")
+            return
+        }
+        
+        imagePicker.sourceType = .camera
+        imagePicker.delegate = self
 
+        present(imagePicker, animated: true)
+    }
+    
+}
+
+extension AddNewBookViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+//    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+//        defer {
+//            picker.dismiss(animated: true)
+//        }
+//
+//        print(info)
+//    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        defer {
+            picker.dismiss(animated: true)
+        }
+        
+        print("did cancel")
+    }
+    
+     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        
+        let image = info[UIImagePickerControllerOriginalImage] as! UIImage
+        
+        self.image = image
+        self.photoUploadImage.image = image
+        
+        print("---------hihihi i hihihi_______")
+        self.checkPhotoUpload.text = "You have uploaded"
+        
+        
+        picker.dismiss(animated:true, completion: nil)
+        
+    }
 }
