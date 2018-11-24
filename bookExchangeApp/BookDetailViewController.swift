8ea4a87c939e0d8ba76eed05dcdd808322ec9c72
15217035 <15217035@cs12202.comp.hkbu.edu.hk>
2018-11-24 19:07:19 +0800

    add comment page

--- bookExchangeApp/BookDetailViewController.swift
+++ bookExchangeApp/BookDetailViewController.swift
@@ -20,7 +20,11 @@ class BookDetailViewController: UIViewController {
     @IBOutlet weak var bookImage: UIImageView!
     
     var book_id:String = "0001"
-
+    
+    
+    @IBOutlet weak var tableComment: UITableView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -74,16 +78,29 @@ class BookDetailViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+
     
 
-    /*
+    
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         // Get the new view controller using segue.destinationViewController.
         // Pass the selected object to the new view controller.
+        
+        
+        if segue.identifier == "showComment" {
+            
+            if let viewController = segue.destination as? CommentTableViewController {
+                
+                
+                
+                viewController.book_id = book_id
+                
+            }
+        }
     }
-    */
+    
 
 }
